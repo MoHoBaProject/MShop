8f9a7b42e25c6c0acf2a6001bc40e58c02621ffa
mohammadhosseinbabaei <mohammadhoseinbabaii1375@gmail.com>
2024-06-04 19:16:49 +0330

    first commit

---
+++ src/main/java/com/common/mshop/MShopApplication.java
@@ -0,0 +1,13 @@
+package com.common.mshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/common/mshop/MShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.mshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
